56d02a200a706a2210691fa1e7675e7ea212a515
EC2 Default User <ec2-user@ip-172-31-57-205.ec2.internal>
2021-11-10 08:09:56 +0000

    InitialCommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   resources :books do
      resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_070601) do
+ActiveRecord::Schema.define(version: 2021_11_09_155809) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_11_09_070601) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_070601) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
